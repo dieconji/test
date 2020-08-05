12f2efab2ae608cd01eeef767aae83ae20d8086b
dieconji <dieconji@gmail.com>
2020-08-05 16:17:52 +0200

    added mysql test

--- src/index.js
+++ src/index.js
@@ -1,45 +1,27 @@
-/*
 const mysql = require('mysql');
-
-
-console.log(mysqlConfig)
-
-const con = mysql.createConnection({
-  host: 'localhost',
-  user: mysqlConfig.user,
-  password: mysqlConfig.password,
-});
-
-con.connect((err) => {
-  if(err){
-    console.log('Error connecting to Db');
-    return;
-  }
-  console.log('Connection established');
-});
-
-con.end((err) => {
-
-});
-*/
 
 (async () => {
   try {
-    console.log('eo');
     const { mysqlConfig } = await require('./vars');
 
-    console.log(mysqlConfig);
+    const con = mysql.createConnection({
+      host: 'localhost',
+      user: mysqlConfig.user,
+      password: mysqlConfig.password,
+    });
+    
+    con.connect((err) => {
+      if(err){
+        console.log('Error connecting to Db');
+        return;
+      }
+      console.log('Connection established');
+    });
+    
+    con.end((err) => {
+      console.log(err);
+    });
   } catch (e) {
     console.log(e);
   }
 })();
-
-/*
-main()
-    .then(text => {
-        console.log('text');
-    })
-    .catch(err => {
-        // Deal with the fact the chain failed
-    });
-    */
